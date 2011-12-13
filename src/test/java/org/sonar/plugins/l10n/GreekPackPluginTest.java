926878c48127b3d642bc638099f45e2020bd763a
Papapetrou Patroklos <ppapapetrou76@gmail.com>
2011-12-13 08:25:42 +0000

    Issue #SONARPLUGINS-1560 Update to include new Sonar 2.12 properties

--- src/test/java/org/sonar/plugins/l10n/GreekPackPluginTest.java
+++ src/test/java/org/sonar/plugins/l10n/GreekPackPluginTest.java
@@ -26,7 +26,7 @@ import org.junit.Test;
 
 public class GreekPackPluginTest {
 
-  private static final String SONAR_VERSION = "2.11";
+  private static final String SONAR_VERSION = "2.12";
 
   @Test
   public void coreBundleShouldBeUpToDate() {
